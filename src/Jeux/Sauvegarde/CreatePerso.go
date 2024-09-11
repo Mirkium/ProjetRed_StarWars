51e741c574e9e7be45f133694161cdf345ffc935
Mirkium <alexandre.petitfrere@ynov.com>
2024-09-11 16:56:58 +0200

    pull

--- src/Jeux/Sauvegarde/CreatePerso.go
+++ src/Jeux/Sauvegarde/CreatePerso.go
@@ -2,43 +2,50 @@ package Sauvegarde
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 )
-
 
 const (
 	Reset   = "\033[0m"
-	Red     = "\033[31m"
+	Red     = "\033[91m"
 	Green   = "\033[32m"
-	Yellow  = "\033[33m"
+	Yellow  = "\033[93m"
 	Blue    = "\033[34m"
 	Magenta = "\033[35m"
-	Cyan    = "\033[36m"
+	Cyan    = "\033[96m"
 	White   = "\033[37m"
+	gray    = "\033[90m"
 )
 
 var Personnage Perso
 
-
-
 func CreatePerso() {
 	fmt.Println(" ▓  █ ▄ ▀ ■  ║")
-	fmt.Println(Yellow, "")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
+	fmt.Println(Yellow, "        ▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█     █▀▀▀▀▀█        █▀▀▀▀▀▀▀▀▀▄         █▀▀▀▀█     █▀▀▀█     █▀▀▀▀█   █▀▀▀▀▀█        █▀▀▀▀▀▀▀▀▀▄      ▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█")
+	fmt.Println(Yellow, "       █                            █    █   ▄   █       █   █▀▀▄   █         █    █   █     █   █    █   █   ▄   █       █   █▀▀▄   █    █                █")
+	fmt.Println(Yellow, "        ▀▄    ▀█▀▀▀▀▀▀▀▀▀█   █▀▀▀▀▀▀▀   █   █ █   █      █   ▀▀▀  ▄▀           █    █ █       █ █    █   █   █ █   █      █   ▀▀▀  ▄▀      ▀▄    ▀█▀▀▀▀▀▀▀▀▀")
+	fmt.Println(Yellow, "          ▀▄    ▀▄       █   █         █   █▄▄▄█   █     █   ▄     ▀▄           █    ▀    ▄    ▀    █   █   █▄▄▄█   █     █   ▄     ▀▄       ▀▄    ▀▄")
+	fmt.Println(Yellow, "      ▄▄▄▄▄▄█▄    ▀▄     █   █        █    ▄▄▄▄▄    █    █   █▀▄     ▀▄▄▄▄▄▄▄    █       █ █       █   █    ▄▄▄▄▄    █    █   █▀▄     ▀▄▄▄▄▄▄▄▄█▄    ▀▄")
+	fmt.Println(Yellow, "      █             █    █   █       █    █     █    █   █   █  ▀▄          █     █     █   █     █   █    █     █    █   █   █  ▀▄                    █")
+	fmt.Println(Yellow, "      █▄▄▄▄▄▄▄▄▄▄▄▄▀     █▄▄▄█      █▄▄▄▄█       █▄▄▄▄█  █▄▄▄█    ▀▄▄▄▄▄▄▄▄▄█      █▄▄▄█     █▄▄▄█   █▄▄▄▄█       █▄▄▄▄█  █▄▄▄█    ▀▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▀")
+	fmt.Println("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+	fmt.Println("                              ", gray, "▄", Reset, "                                      ", Yellow, "PROJET  RED", Reset, "                                      ", gray, "▄", Reset)
+	fmt.Println("                              ", gray, "▓", Reset, "                                                                                           ", gray, "▓", Reset)
+	fmt.Println("                              ", gray, "▓", Reset, "                                                                                           ", gray, "▓", Reset)
+	fmt.Println("                              ", gray, "█", Reset, "                                ", Cyan, "1 -  ", Yellow, "NEW  GAME", Reset, "                                       ", gray, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                                                                           ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                                                                           ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                ", Cyan, "2 -  ",  Yellow, "SAVE GAME", Reset, "                                       ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                                                                           ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                                                                           ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█", Reset, "                                ", Cyan, "3 -  ",  Yellow, "  QUIT", Reset, "                                          ", Red, "█", Reset)
+	fmt.Println("                              ", Red, "█                                                                                               █", Reset)
+	fmt.Println("                              ", Red, "█                                                                                               █", Reset)
+	fmt.Println("                              ", Red, "█                                                                                               █", Reset)
+	fmt.Println("                              ", Red, "█                                                                                               █", Reset)
 }
-
-
 
 func clearScreen() {
 	var cmd *exec.Cmd
--- src/main.go
+++ src/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	save "Game/Jeux/Sauvegarde"
-	game "Game/Jeux/GamePlay"
-	marchand "Game/Jeux/GamePlay/Marchand"
-	forgeron "Game/Jeux/GamePlay/Forgeron"
-	guerrierSith "Game/Jeux/GamePlay/FirstMission/Sith/GuerrierSith"
-	assassinSith "Game/Jeux/GamePlay/FirstMission/Sith/AssassinSith"
-	chevalierJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ChevalierJedi"
-	consulaireJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ConsulaireJedi"
+	//game "Game/Jeux/GamePlay"
+	//marchand "Game/Jeux/GamePlay/Marchand"
+	//forgeron "Game/Jeux/GamePlay/Forgeron"
+	//guerrierSith "Game/Jeux/GamePlay/FirstMission/Sith/GuerrierSith"
+	//assassinSith "Game/Jeux/GamePlay/FirstMission/Sith/AssassinSith"
+	//chevalierJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ChevalierJedi"
+	//consulaireJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ConsulaireJedi"
 )
 
 func main() {
 	save.CreatePerso()
 }
-
