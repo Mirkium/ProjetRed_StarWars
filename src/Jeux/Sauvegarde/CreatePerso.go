f9428e34d2e54871aa58488f99a4d7346efdac88
Mirkium <alexandre.petitfrere@ynov.com>
2024-09-11 15:26:05 +0200

    main update

--- src/Jeux/Sauvegarde/CreatePerso.go
+++ src/Jeux/Sauvegarde/CreatePerso.go
@@ -1,5 +1,57 @@
 package Sauvegarde
 
-func CreatePerso() {
+import (
+	"fmt"
+)
 
+
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	White   = "\033[37m"
+)
+
+var Personnage Perso
+
+
+
+func CreatePerso() {
+	fmt.Println(" ▓  █ ▄ ▀ ■  ║")
+	fmt.Println(Yellow, "")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+}
+
+
+
+func clearScreen() {
+	var cmd *exec.Cmd
+	// Détecter le système d'exploitation
+	switch runtime.GOOS {
+	case "windows":
+		// Commande pour Windows
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		// Commande pour les systèmes Unix-like
+		cmd = exec.Command("clear")
+	}
+	// Définir la sortie de la commande sur Stdout
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
--- src/main.go
+++ src/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	save "Game/Jeux/Sauvegarde"
+	game "Game/Jeux/GamePlay"
+	marchand "Game/Jeux/GamePlay/Marchand"
+	forgeron "Game/Jeux/GamePlay/Forgeron"
+	guerrierSith "Game/Jeux/GamePlay/FirstMission/Sith/GuerrierSith"
+	assassinSith "Game/Jeux/GamePlay/FirstMission/Sith/AssassinSith"
+	chevalierJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ChevalierJedi"
+	consulaireJedi "Game/Jeux/GamePlay/FirstMission/Jedi/ConsulaireJedi"
 )
 
 func main() {
 	save.CreatePerso()
 }
+
