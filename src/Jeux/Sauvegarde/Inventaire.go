53b3b2fe9d41b16db67c938bc4eb555c5f5557c1
Adrien LECOMTE <adrien@lecomte.org>
2024-09-12 11:40:37 +0200

    ajout de l'interface de l'inventaire

--- src/Jeux/Sauvegarde/Inventaire.go
+++ src/Jeux/Sauvegarde/Inventaire.go
@@ -1,6 +1,9 @@
 package Sauvegarde
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var Inventaire map[ItemPacket]int = make(map[ItemPacket]int)
 
@@ -54,11 +57,36 @@ func DisplayInventaire() {
 	/*
 		Affiche les diiferents item
 	*/
+	clearScreen()
 	i := 1
+	fmt.Println(" ========================Inventaire=========================")
 	for index, element := range Inventaire {
-		fmt.Printf("Item :  %s, Quantité : %d, Index : %d\n", index.Objet.Name, element, i)
+		fmt.Printf("|Index : %d | Item :  %s | Quantité : %s |\n", i, formatage(index.Objet.Name, 16), formatage(string(element+48), 4))
 		i++
 	}
+	fmt.Println(" ===========================================================")
+}
+
+func formatage(c string, z int) string {
+	res := ""
+	l := len(c)
+	n := (z - l) / 2
+	if l%2 == 1 {
+		res += " "
+	}
+	for i := 0; i < n; i++ {
+		res += " "
+	}
+	res += c
+	for i := 0; i <= n+1; i++ {
+		res += " "
+	}
+	return res
+}
+
+func formatagePrix(prix int) string {
+	res := strconv.Itoa(prix)
+	return formatage(res+" credits", 16)
 }
 
 func DisplayInventaireIndex(index1 int) {
@@ -68,19 +96,60 @@ func DisplayInventaireIndex(index1 int) {
 		i := 1
 		for index, element := range Inventaire {
 			if index1 == i {
-				fmt.Printf("Item :  %s, Quantité : %d\n", index.Objet.Name, element)
+				clearScreen()
+				fmt.Println(" ========================Inventaire=========================")
+				fmt.Println("|--------------------------Name-----------------------------|")
+				fmt.Println("|                                                           |")
+				fmt.Println("|                    " + formatage(index.Objet.Name, 16) + "                     |")
+				fmt.Println("|                                                           |")
+				fmt.Println("|-----------------------Description-------------------------|")
+				fmt.Println("|                                                           |")
+				fmt.Println("|   " + index.Objet.Description)
+				fmt.Println("|                                                           |")
+				fmt.Println("---------------------------Prix-----------------------------|")
+				fmt.Println("|                                                           |")
+				fmt.Println("|                     " + formatagePrix(index.Objet.Valeur) + "                    |")
+				fmt.Println("|                                                           |")
+				fmt.Println("|------------------------Quantites--------------------------|")
+				fmt.Println("|                                                           |")
+				fmt.Println("|                          " + formatage(strconv.Itoa(element), 3) + "                           |")
+				fmt.Println("|                                                           |")
+				fmt.Println(" ===========================================================")
+				//fmt.Printf("Item :  %s, Quantité : %d\n", index.Objet.Name, element)
 			}
 			i++
 		}
 	}
 }
 
-func Selectionner() {
-	/*
-		Montre les inforamtion de l'item selectionner.
-		Gere les autres cas d'ereur.
-	*/
+func Vendre(index int, quantite int) {
 	var choix string
+	DisplayInventaire()
+	fmt.Println("Quel item veut tu vendre ?")
+	fmt.Scanln(&choix)
+	if len(choix) != 1 {
+		fmt.Println("error 404 : pas le bon input")
+	}
+	for _, let := range choix {
+		if let >= '0' && let <= '9' {
+			//DisplayInventaireIndex(int(let - 48))
+			i := 1
+			for key := range Inventaire {
+				if index == i {
+					Enlever_Item(key.Objet, quantite)
+					//return value * quantite
+				}
+			}
+		} else {
+			fmt.Println("on t'a demandé un chiffre pas autre chose.")
+		}
+	}
+}
+
+func RegardeStat() {
+	DisplayInventaire()
+	var choix string
+	fmt.Println("Veux tu voir plus de détails sur un item (si non rentre juste 0)")
 	fmt.Scanln(&choix)
 	if len(choix) != 1 {
 		fmt.Println("error 404 : pas le bon input")
--- src/Jeux/Sauvegarde/Item.go
+++ src/Jeux/Sauvegarde/Item.go
@@ -3,5 +3,5 @@ package Sauvegarde
 /*
 Ficher pour stocker les differents items
 */
-var Sabre_laser Item = Item{"sabre laser", 100, "Sabre laser ayant appartenu à Dark Plaigis lui-même"}
+var Sabre_laser Item = Item{"sabre laser", 1000, "Le sabre laser était une arme de mêlée principalement   |\n|     utilisée par les Jedi et les Sith. Il s'agissait      |\n|      d'une sorte d'épée possédant une lame de plasma      |\n|     alimentée par un cristal kyber et émise depuis une    |\n|        poignée métallique. Cette arme nécessitait un      |\n|      entraînement et son utilisation était améliorée      |\n|     lorsque son utilisateur avait recours à la Force.     |\n|           Bien qu'il fut aussi manié par les Sith,        |\n|          le sabre laser est le symbole des Jedi.          |"}
 var Casque Item = Item{"Casque", 20, "Casque appartenant au gardien du temple jedi."}
